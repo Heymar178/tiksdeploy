464a8f694009c2bd56d7e4b8ebe4e8bb6a52953c
Heymar178 <hamzaworks178@gmail.com>
2025-03-01 05:10:06 -0600

    Add files via upload

---
+++ src/screens/Wireframe/Wireframe.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+
+export const Wireframe = () => {
+  return (
+    <div className="bg-white flex flex-row justify-center w-full">
+      <div className="bg-white w-[1440px] h-[1326px] relative">
+        <img
+          className="absolute w-[77px] h-[103px] top-6 left-[1308px]"
+          alt="Image"
+          src="/img/image-9.png"
+        />
+
+        <img
+          className="top-[272px] left-[33px] absolute w-[600px] h-0.5"
+          alt="Line"
+          src="/img/line-2.svg"
+        />
+
+        <img
+          className="top-[622px] left-11 absolute w-[600px] h-0.5"
+          alt="Line"
+          src="/img/line-3.svg"
+        />
+
+        <img
+          className="top-[272px] left-[785px] absolute w-[600px] h-0.5"
+          alt="Line"
+          src="/img/line-2.svg"
+        />
+
+        <div className="absolute w-[523px] h-[158px] top-[65px] left-[458px] [font-family:'Cabin_Sketch',Helvetica] font-bold text-[#1d1d1b] text-[80px] text-center tracking-[-2.40px] leading-[80px]">
+          TIKS DEPLOY
+        </div>
+
+        <div className="absolute w-[155px] h-[50px] top-[662px] left-11 [font-family:'Cabin_Sketch',Helvetica] font-bold text-[#1d1d1b] text-[40px] text-center tracking-[-1.20px] leading-10">
+          Example:
+        </div>
+
+        <img
+          className="absolute w-[362px] h-[477px] top-[752px] left-[133px]"
+          alt="Image"
+          src="/img/image-28.png"
+        />
+
+        <img
+          className="absolute w-48 h-[301px] top-72 left-[103px]"
+          alt="Image"
+          src="/img/image-27.png"
+        />
+
+        <img
+          className="absolute w-[239px] h-[272px] top-[303px] left-[344px]"
+          alt="Image"
+          src="/img/image-20.png"
+        />
+
+        <div className="absolute w-[156px] h-[39px] top-[295px] left-[1007px] [font-family:'Bubblegum_Sans',Helvetica] font-normal text-[#1d1d1b] text-[25px] text-center tracking-[-0.75px] leading-[25px]">
+          Release Notes
+        </div>
+
+        <p className="absolute w-[610px] h-[39px] top-[399px] left-[780px] [font-family:'Bubblegum_Sans',Helvetica] font-normal text-[#1d1d1b] text-[25px] text-center tracking-[-0.75px] leading-[25px]">
+          Post a tiktok @tiksdepoy with any media and put the name you’d want
+          <br />
+          the memecoin to be and it’ll create visuals for you.
+        </p>
+
+        <p className="absolute w-[610px] h-[39px] top-[487px] left-[780px] [font-family:'Bubblegum_Sans',Helvetica] font-normal text-[#1d1d1b] text-[25px] text-center tracking-[-0.75px] leading-[25px]">
+          Better stability under heavy workload
+        </p>
+
+        <p className="absolute w-[636px] h-[39px] top-[740px] left-[774px] [font-family:'Bubblegum_Sans',Helvetica] font-normal text-[#1d1d1b] text-[25px] text-center tracking-[-0.75px] leading-[25px]">
+          Resolved an issue that caused blank images to be generated frequently
+          due to moderation.
+        </p>
+
+        <div className="absolute w-[610px] h-[39px] top-[675px] left-[780px] [font-family:'Bubblegum_Sans',Helvetica] font-normal text-[#1d1d1b] text-[25px] text-center tracking-[-0.75px] leading-[25px]">
+          Situational Content Design
+        </div>
+
+        <p className="absolute w-[610px] h-[39px] top-[805px] left-[780px] [font-family:'Bubblegum_Sans',Helvetica] font-normal text-[#1d1d1b] text-[25px] text-center tracking-[-0.75px] leading-[25px]">
+          Fixed an issue that was causing the tiktok link to not appear
+        </p>
+
+        <p className="absolute w-[610px] h-[39px] top-[872px] left-[780px] [font-family:'Bubblegum_Sans',Helvetica] font-normal text-[#1d1d1b] text-[25px] text-center tracking-[-0.75px] leading-[25px]">
+          The more details you provide, the better the output.
+        </p>
+
+        <p className="absolute w-[610px] h-[39px] top-[1012px] left-[780px] [font-family:'Bubblegum_Sans',Helvetica] font-normal text-[#1d1d1b] text-[25px] text-center tracking-[-0.75px] leading-[25px]">
+          Minimized token creation during discussions
+          <br />
+          with @tiksdeploy
+        </p>
+
+        <div className="absolute w-[610px] h-[39px] top-[554px] left-[780px] [font-family:'Bubblegum_Sans',Helvetica] font-normal text-[#1d1d1b] text-[25px] text-center tracking-[-0.75px] leading-[25px]">
+          Various bugs fixed
+        </div>
+
+        <div className="top-[341px] absolute w-[156px] h-[39px] left-[1007px] [font-family:'Brygada_1918',Helvetica] font-semibold text-[#1d1d1b] text-[25px] text-center tracking-[-0.75px] leading-[25px]">
+          V 1.2.0
+        </div>
+
+        <div className="top-[615px] absolute w-[156px] h-[39px] left-[1007px] [font-family:'Brygada_1918',Helvetica] font-semibold text-[#1d1d1b] text-[25px] text-center tracking-[-0.75px] leading-[25px]">
+          V 1.3.0
+        </div>
+
+        <div className="top-[951px] absolute w-[156px] h-[39px] left-[1007px] [font-family:'Brygada_1918',Helvetica] font-semibold text-[#1d1d1b] text-[25px] text-center tracking-[-0.75px] leading-[25px]">
+          V 1.4.0
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,135 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        getkobememescomblack: "var(--getkobememescomblack)",
+        getkobememescombuttercup: "var(--getkobememescombuttercup)",
+        "getkobememescomcod-gray": "var(--getkobememescomcod-gray)",
+        getkobememescomgigas: "var(--getkobememescomgigas)",
+        "getkobememescomgold-tips": "var(--getkobememescomgold-tips)",
+        getkobememescomjacarta: "var(--getkobememescomjacarta)",
+        getkobememescommartinique: "var(--getkobememescommartinique)",
+        "getkobememescommine-shaft": "var(--getkobememescommine-shaft)",
+        "getkobememescomschool-bus-yellow":
+          "var(--getkobememescomschool-bus-yellow)",
+        getkobememescomtango: "var(--getkobememescomtango)",
+        getkobememescomtuscany: "var(--getkobememescomtuscany)",
+        getkobememescomwhite: "var(--getkobememescomwhite)",
+        getkobememescomwisteria: "var(--getkobememescomwisteria)",
+        laboaitechcomanakiwa: "var(--laboaitechcomanakiwa)",
+        laboaitechcomapricot: "var(--laboaitechcomapricot)",
+        "laboaitechcomdodger-blue": "var(--laboaitechcomdodger-blue)",
+        laboaitechcommauve: "var(--laboaitechcommauve)",
+        laboaitechcomsandwisp: "var(--laboaitechcomsandwisp)",
+        laboaitechcomthunder: "var(--laboaitechcomthunder)",
+        officialelonmemecomwhite: "var(--officialelonmemecomwhite)",
+        wwwsketchymcdrawcomblack: "var(--wwwsketchymcdrawcomblack)",
+        "wwwsketchymcdrawcomdodger-blue":
+          "var(--wwwsketchymcdrawcomdodger-blue)",
+        wwwsketchymcdrawcomjade: "var(--wwwsketchymcdrawcomjade)",
+        wwwsketchymcdrawcommalibu: "var(--wwwsketchymcdrawcommalibu)",
+        wwwsketchymcdrawcomporcelain: "var(--wwwsketchymcdrawcomporcelain)",
+        "wwwsketchymcdrawcomregent-gray":
+          "var(--wwwsketchymcdrawcomregent-gray)",
+        wwwsketchymcdrawcomrose: "var(--wwwsketchymcdrawcomrose)",
+        wwwsketchymcdrawcomwhite: "var(--wwwsketchymcdrawcomwhite)",
+        wwwvinecoinwhite: "var(--wwwvinecoinwhite)",
+      },
+      fontFamily: {
+        "cr7memeland-com-arial-bold":
+          "var(--cr7memeland-com-arial-bold-font-family)",
+        "cr7memeland-com-arial-narrow":
+          "var(--cr7memeland-com-arial-narrow-font-family)",
+        "getkobememes-com-bebas-neue-regular":
+          "var(--getkobememes-com-bebas-neue-regular-font-family)",
+        "getkobememes-com-bebas-neue-regular-upper":
+          "var(--getkobememes-com-bebas-neue-regular-upper-font-family)",
+        "laboaitech-com-arial-bold":
+          "var(--laboaitech-com-arial-bold-font-family)",
+        "laboaitech-com-arial-narrow":
+          "var(--laboaitech-com-arial-narrow-font-family)",
+        "laboaitech-com-inter-bold":
+          "var(--laboaitech-com-inter-bold-font-family)",
+        "laboaitech-com-inter-medium":
+          "var(--laboaitech-com-inter-medium-font-family)",
+        "laboaitech-com-semantic-heading-1":
+          "var(--laboaitech-com-semantic-heading-1-font-family)",
+        "laboaitech-com-semantic-heading-2":
+          "var(--laboaitech-com-semantic-heading-2-font-family)",
+        "laboaitech-com-semantic-link":
+          "var(--laboaitech-com-semantic-link-font-family)",
+        "laboaitech-com-semantic-pre":
+          "var(--laboaitech-com-semantic-pre-font-family)",
+        "laboaitech-com-zen-kaku-gothic-antique-black":
+          "var(--laboaitech-com-zen-kaku-gothic-antique-black-font-family)",
+        "laboaitech-com-zen-kaku-gothic-antique-bold":
+          "var(--laboaitech-com-zen-kaku-gothic-antique-bold-font-family)",
+        "laboaitech-com-zen-kaku-gothic-antique-light":
+          "var(--laboaitech-com-zen-kaku-gothic-antique-light-font-family)",
+        "laboaitech-com-zen-kaku-gothic-antique-medium":
+          "var(--laboaitech-com-zen-kaku-gothic-antique-medium-font-family)",
+        "officialelonmeme-com-font-awesome-5-free-solid":
+          "var(--officialelonmeme-com-font-awesome-5-free-solid-font-family)",
+        "officialelonmeme-com-roboto-condensed-regular":
+          "var(--officialelonmeme-com-roboto-condensed-regular-font-family)",
+        "officialelonmeme-com-semantic-heading-2":
+          "var(--officialelonmeme-com-semantic-heading-2-font-family)",
+        "officialelonmeme-com-semantic-link":
+          "var(--officialelonmeme-com-semantic-link-font-family)",
+        "www-falconpulse-ai-inter-bold":
+          "var(--www-falconpulse-ai-inter-bold-font-family)",
+        "www-falconpulse-ai-inter-regular":
+          "var(--www-falconpulse-ai-inter-regular-font-family)",
+        "www-falconpulse-ai-semantic-heading-1":
+          "var(--www-falconpulse-ai-semantic-heading-1-font-family)",
+        "www-falconpulse-ai-semantic-heading-2":
+          "var(--www-falconpulse-ai-semantic-heading-2-font-family)",
+        "www-falconpulse-ai-semantic-heading-3":
+          "var(--www-falconpulse-ai-semantic-heading-3-font-family)",
+        "www-falconpulse-ai-semantic-link":
+          "var(--www-falconpulse-ai-semantic-link-font-family)",
+        "www-falconpulse-ai-source-sans-3-light":
+          "var(--www-falconpulse-ai-source-sans-3-light-font-family)",
+        "www-falconpulse-ai-source-sans-3-regular":
+          "var(--www-falconpulse-ai-source-sans-3-regular-font-family)",
+        "www-sketchymcdraw-com-consolas-regular":
+          "var(--www-sketchymcdraw-com-consolas-regular-font-family)",
+        "www-sketchymcdraw-com-segoe-UI-bold":
+          "var(--www-sketchymcdraw-com-segoe-UI-bold-font-family)",
+        "www-sketchymcdraw-com-segoe-UI-bold-underline":
+          "var(--www-sketchymcdraw-com-segoe-UI-bold-underline-font-family)",
+        "www-sketchymcdraw-com-segoe-UI-italic":
+          "var(--www-sketchymcdraw-com-segoe-UI-italic-font-family)",
+        "www-sketchymcdraw-com-segoe-UI-regular":
+          "var(--www-sketchymcdraw-com-segoe-UI-regular-font-family)",
+        "www-sketchymcdraw-com-segoe-UI-regular-underline":
+          "var(--www-sketchymcdraw-com-segoe-UI-regular-underline-font-family)",
+        "www-sketchymcdraw-com-segoe-UI-semibold":
+          "var(--www-sketchymcdraw-com-segoe-UI-semibold-font-family)",
+        "www-sketchymcdraw-com-semantic-heading-1":
+          "var(--www-sketchymcdraw-com-semantic-heading-1-font-family)",
+        "www-sketchymcdraw-com-semantic-heading-2":
+          "var(--www-sketchymcdraw-com-semantic-heading-2-font-family)",
+        "www-sketchymcdraw-com-semantic-heading-3":
+          "var(--www-sketchymcdraw-com-semantic-heading-3-font-family)",
+        "www-sketchymcdraw-com-semantic-item":
+          "var(--www-sketchymcdraw-com-semantic-item-font-family)",
+        "www-sketchymcdraw-com-semantic-label":
+          "var(--www-sketchymcdraw-com-semantic-label-font-family)",
+        "www-sketchymcdraw-com-semantic-textarea":
+          "var(--www-sketchymcdraw-com-semantic-textarea-font-family)",
+        "www-vineco-in-arial-bold":
+          "var(--www-vineco-in-arial-bold-font-family)",
+        "www-vineco-in-arial-narrow":
+          "var(--www-vineco-in-arial-narrow-font-family)",
+        "www-vineco-in-semantic-link":
+          "var(--www-vineco-in-semantic-link-font-family)",
+        "www-vineco-in-semantic-link-underline":
+          "var(--www-vineco-in-semantic-link-underline-font-family)",
+      },
+    },
+  },
+  plugins: [],
+};
